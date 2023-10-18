7b73b5544073fcc6180e2b4585ec8fd03d2b9a0e
ShIIIrochka <shiroooooo@yandex.ru>
2023-10-18 23:05:02 +0300

    создала спрайты, добавила движение спрайтов

--- pythongame/gamegame.py
+++ pythongame/gamegame.py
@@ -1,25 +1,28 @@
 # Pygame шаблон - скелет для нового проекта Pygame
 
 import pygame
+from sys import exit
 
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
-
-# Задаем цвета
-
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-
-# Создаем игру и окноls
-
+WIDTH = 728
+HEIGHT = 350
+FPS = 60
 
 pygame.init()
-pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+#load and convert images
+sky_surface = pygame.image.load('/home/ksenia/projects/py_game/pythongame/sky.PNG').convert()
+ground_surface = pygame.image.load('/home/ksenia/projects/py_game/pythongame/ground.PNG').convert()
+
+pit_surface = pygame.image.load('/home/ksenia/projects/py_game/pythongame/pit.PNG').convert_alpha()
+pit_rect = pit_surface.get_rect(midbottom  = (350, 250))
+
+cat_surf = pygame.image.load('/home/ksenia/projects/py_game/pythongame/cat.PNG').convert_alpha()
+
+player_surf = pygame.transform.scale(cat_surf, (70, 70))
+player_rect = player_surf.get_rect(midbottom = (100,250))
+player_gravity = 0
+
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
 
@@ -30,38 +33,43 @@ running = True
 while running:
 
     # Держим цикл на правильной скорости
-    
     clock.tick(FPS)
-
-    # Ввод процесса (события)
-
+    # ввод процесса (события)
     for event in pygame.event.get():
-
-        # check for closing window
-
+    
         if event.type == pygame.QUIT:
-            running = False
+                pygame.quit()
+                exit()
+
+        if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    exit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rect.collidepoint(event.pos):
+                player_gravity = -20
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player_gravity = -20
+
+    screen.blit(sky_surface, (0,0)) #вывод на экран изображения неба
+    screen.blit(ground_surface, (0, 250)) #вывод на экран изображения земли
+
+    player_gravity += 1
+    player_rect.y += player_gravity
+    if player_rect.bottom >= 250: player_rect.bottom = 250
+    screen.blit(player_surf, player_rect)
+
+    pit_rect.x -= 5
+    if pit_rect.right <= 0: pit_rect.left = 728
+    screen.blit(pit_surface, pit_rect)
+
+    mouse_pos = pygame.mouse.set_visible(True)
+
+    #if player_rect.colliderect(pit_rect) == 1: 
+     #   print("True")
 
 
-while running:
-    # держим цикл на правильной скорости
-    clock.tick(FPS)
-
-#создание спрайтов
-
-clock = pygame.time.Clock()
-all_sprites = pygame.sprite.Group() 
-
-all_sprites.update()
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
-        self.image.filShIIIrochkal(GREEN)
-        self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
-
-all_sprites = pygame.sprite.Group()
-player = Player()
-all_sprites.add(player)
+    pygame.display.update()
