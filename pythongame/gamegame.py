c7341ecdd22d058b55b958fe876cc92072831379
ShIIIrochka <shiroooooo@yandex.ru>
2023-10-19 04:58:39 +0300

    добавила рыбку, ее движение, экран старта

--- pythongame/gamegame.py
+++ pythongame/gamegame.py
@@ -1,89 +1,102 @@
-# Pygame шаблон - скелет для нового проекта Pygame
-
 import pygame
 from sys import exit
-
+ 
 WIDTH = 728
 HEIGHT = 350
 FPS = 60
-
+ 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+ 
+# Загрузка и конвертация изображений
+sky_surface = pygame.image.load('sky.PNG').convert()
+ground_surface = pygame.image.load('ground.PNG').convert()
+ 
+pit1_surface = pygame.image.load('pit.PNG').convert_alpha()
+pit_surf = pygame.transform.scale(pit1_surface, (45, 45))
+pit_rect = pit_surf.get_rect(midbottom=(728, 270))
+ 
+cat_surf = pygame.image.load('cat.PNG').convert_alpha()
 
-#load and convert images
-sky_surface = pygame.image.load('/home/ksenia/projects/py_game/pythongame/sky.PNG').convert()
-ground_surface = pygame.image.load('/home/ksenia/projects/py_game/pythongame/ground.PNG').convert()
+cat_stand_surf = pygame.image.load('cat.stand.PNG').convert_alpha()
+cat_stand_surf = pygame.transform.scale(cat_stand_surf, (200, 200))
+cat_rect = cat_stand_surf.get_rect(midbottom = (364, 230))
 
-pit1_surface = pygame.image.load('/home/ksenia/projects/py_game/pythongame/pit.PNG').convert_alpha()
-pit_surf = pygame.transform.scale(pit1_surface, (50, 50))
-pit_rect = pit_surf.get_rect(midbottom  = (728, 250))
+bird_surf = pygame.image.load('fish.PNG').convert_alpha()
+bird_surf = pygame.transform.scale(bird_surf, (50, 50))
+bird_rect= bird_surf.get_rect(midbottom=(728, 70))
 
-cat_surf = pygame.image.load('/home/ksenia/projects/py_game/pythongame/cat.PNG').convert_alpha()
-
-player_surf = pygame.transform.scale(cat_surf, (70, 70))
-player_rect = player_surf.get_rect(midbottom = (100,250))
+player_surf = pygame.transform.scale(cat_surf, (90, 90))
+player_rect = player_surf.get_rect(midbottom=(100, 250))
 player_gravity = 0
-
+ 
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
-
-# Цикл игры
-
+ 
+# Флаги для состояния игры
 running = True
-game_acrive = True
-
+game_active = False
+ 
 while running:
-
     # Держим цикл на правильной скорости
     clock.tick(FPS)
-    # ввод процесса (события)
+    # Обработка ввода (событий)
     for event in pygame.event.get():
-        if game_acrive:
-            if event.type == pygame.QUIT:
-                    pygame.quit()
-                    exit()
 
-            if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        pygame.quit()
-                        exit()
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
+
+        if game_active:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if player_rect.collidepoint(event.pos):
                     player_gravity = -20
-
+ 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     player_gravity = -20
         else:
-            #рестарт, если игрок проиграл
-            if event.type == pygame.KEYDOWN and event.type == event.key == pygame.K_SPACE:
-                game_acrive = True
-                pit_rect.left = 750
-
-
-    if game_acrive:
-        screen.blit(sky_surface, (0,0)) #вывод на экран изображения неба
-        screen.blit(ground_surface, (0, 250)) #вывод на экран изображения земли
-
-        player_gravity += 1 #скорость кота
+            # Перезапуск игры, если игрок проиграл
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                pit_rect.left = 728
+ 
+    if game_active:
+        screen.blit(sky_surface, (0, 0))  # Вывод изображения неба на экран
+        screen.blit(ground_surface, (0, 250))  # Вывод изображения земли на экран
+ 
+        player_gravity += 1  # Скорость гравитации для кота
         player_rect.y += player_gravity
-        if player_rect.bottom >= 250: player_rect.bottom = 250
+        if player_rect.bottom >= 250:
+            player_rect.bottom = 250
         screen.blit(player_surf, player_rect)
-
+ 
         pit_rect.x -= 5
-        if pit_rect.right <= 0: pit_rect.left = 728
+        if pit_rect.right <= 0:
+            pit_rect.left = 728
         screen.blit(pit_surf, pit_rect)
 
-        mouse_pos = pygame.mouse.set_visible(True)
 
-        #collision
+        bird_rect.x -= 4
+        if bird_rect.right <0:
+            bird_rect.left = 728
+        screen.blit(bird_surf, bird_rect)
 
-        if player_rect.colliderect(pit_rect) == 1: 
-            game_acrive = False
+        # Переключение видимости указателя мыши
+        pygame.mouse.set_visible(True)
+ 
+        # Обработка столкновения
+        if player_rect.colliderect(pit_rect):
+            game_active = False
     else:
-        #если произошло столкновение ямы и кота
-        screen.fill('Green')
-        game_acrive = False
-
+        # Если произошло столкновение кота с ямой
+        screen.fill((123, 145, 123))  # Заполняем экран зеленым цветом
+        screen.blit(cat_stand_surf, cat_rect)
+        game_active = False
+ 
     pygame.display.update()
