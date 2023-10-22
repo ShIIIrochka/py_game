d837a55874b9f6287db248bb9e71d1f1facc0854
ShIIIrochka <shiroooooo@yandex.ru>
2023-10-22 16:19:46 +0300

    доделываю классы

--- pythongame/game.py
+++ pythongame/game.py
@@ -2,25 +2,11 @@ import pygame
 from sys import exit
 from random import randint
  
-WIDTH = 728
-HEIGHT = 350
-FPS = 60
- 
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
- 
-# Загрузка и конвертация изображений
-sky_surface = pygame.image.load('sky.PNG').convert()
-ground_surface = pygame.image.load('ground.PNG').convert()
-
-
-
-
-class Player:
+class Player(pygame.sprite.Sprite):
     """
     функции для игрока - спрайта кота
     """
-    def __init__(self, player_file, size, pos, gravity):
+    def __init__(self, player_file, size, pos):
                 
         """
         :param player_file: имя файла
@@ -28,8 +14,6 @@ class Player:
         :param pos: расположение игрока
         :param gravity: скорось гравитации для кота
         """
-
-        self.gravity = gravity
         self.gravity = 0       
         self.player_surf = pygame.image.load(player_file).convert_alpha()
         self.player_surf = pygame.transform.scale(self.player_surf, size)
@@ -45,7 +29,7 @@ class Player:
     def space_bottom(self):
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE] and self.player_rect.bottom >= 300:
+        if keys[pygame.K_SPACE] and self.player_rect.bottom >= 250:
             self.gravity = -20
 
     def update(self):
@@ -53,11 +37,8 @@ class Player:
         self.space_bottom()
 
 
-
-
-
-class NPS:
-    def __init__(self, file_name, scale, midbottom, score):
+class NPS(pygame.sprite.Sprite):
+    def __init__(self, file_name, scale, midbottom):
                 
         """
         :param file_name: имя файла
@@ -66,54 +47,71 @@ class NPS:
         :param score: счетчик рыбок
         """
                 
-        self.player_surf = pygame.image.load(file_name).convert_alpha()
-        self.player_surf = pygame.transform.scale(self.surf, scale)
-        self.player_rect = self.surf.get_rect(midbottom = midbottom)
+        self.surf = pygame.image.load(file_name).convert_alpha()
+        self.surf = pygame.transform.scale(self.surf, scale)
+        self.rect = self.surf.get_rect(midbottom = midbottom)
 
-        self.score = score
-        self.score = 0
-
-    def move(self, speed):
+    #def move(self, speed):
 
         """
         :param speed: скорость спрайта
-
         """
 
-        self.rect.x -= speed
-        if self.rect.right <= 0:
-            self.rect.left = 728
-        screen.blit(self.surf, self.rect)
+     #   self.rect.x -= speed
+      #  if self.rect.right <= 0:
+       #     self.rect.left = WIDTH
 
-    def score_play(self): 
+    def update(self):
+        self.rect.x -= 6
+        self.destroy()
 
-        if self.player_rect.colliderect(self.rect) == True:
-            self.score += 1
-            
-
-        text = pygame.font.Font(None, 50)
-        score_surf = text.render(f'Score: {score}',False,(64,64,64))
-        score_rect = score_surf.get_rect(center = (364,50))
-
-        screen.blit(score_surf,score_rect)
-        return score
-        
+    def destroy(self):
+        if self.rect.x <= -100: 
+            self.kill()
 
 
 
+def collision_sprite():
+	if pygame.sprite.spritecollide(player.sprite,obstacle_group,False):
+		obstacle_group.empty()
+		return False
+	else: return True
 
+def score_play(): 
+    global score
+    if player.colliderect(fish):
+        score += 1
+
+    text = pygame.font.Font(None, 50)
+    score_surf = text.render(f'Score: {score}',False,(64,64,64))
+    score_rect = score_surf.get_rect(center = (364,50))
+
+    screen.blit(score_surf,score_rect)
+    return score
+
+WIDTH = 728
+HEIGHT = 350
+FPS = 60
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+
+score = 0
+
+# Загрузка и конвертация изображений
+sky_surface = pygame.image.load('sky.PNG').convert()
+ground_surface = pygame.image.load('ground.PNG').convert()
+
+player = Player('cat.PNG', (90, 90), (100, 250))
 
 pit = NPS('pit.PNG', (45, 45), (728, 270))
 start = NPS('cat.stand.PNG', (200, 200), (364, 230))
 fish = NPS('fish.PNG', (50, 50), (728, 70))
-player = Player('cat.PNG', (90, 90), (100, 250))
-
-player_gravity = 0
  
-pygame.display.set_caption("My Game")
-clock = pygame.time.Clock()
-
-
+obstacle_group = pygame.sprite.Group()
+player = pygame.sprite.GroupSingle()
 
 # Флаги для состояния игры
 running = True
@@ -135,38 +133,32 @@ while running:
                 exit()
 
         if game_active:
-            if event.type == pygame.MOUSEBUTTONDOWN and player_rect.y >= 150:
-                if player_rect.collidepoint(event.pos):
-                    player_gravity = -20
- 
-            if event.type == pygame.KEYDOWN and player_rect.y >= 150:
-                if event.key == pygame.K_SPACE:
-                    player_gravity = -20
+            print("hello")
         else:
             # Перезапуск игры, если игрок проиграл
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
-                pit_rect.left = 728
+                score = 0
  
     if game_active:
         screen.blit(sky_surface, (0, 0))  # Вывод изображения неба на экран
         screen.blit(ground_surface, (0, 250))  # Вывод изображения земли на экран
- 
+        score = score_play()
 
+        player.draw(screen)
+        player.update()
+
+        obstacle_group.draw(screen)
+        obstacle_group.update()
+
+        game_active = collision_sprite()
 
         # Переключение видимости указателя мыши
         pygame.mouse.set_visible(True)
 
-        score = score_play()
-
-        # Обработка столкновения
-        if player_rect.colliderect(pit_rect):
-            game_active = False
     else:
         # Если произошло столкновение кота с ямой
         screen.fill((123, 145, 123))  # Заполняем экран зеленым цветом
-        screen.blit(cat_stand_surf, cat_rect)
-        game_active = False
  
     pygame.display.update()
 
